48322b250a023f76d154f0580ba92f337aa18db5
saloniamatteo <saloniamatteo@pm.me>
2025-09-24 15:08:29 +0200

    6.16.7-gentoo -> 6.16.8-gentoo

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ CUSTDIR="/usr/src/usr-kernel"
 
 # Kernel versions
 # NOTE: $KVER MUST be of the form x.y.z, otherwise things WILL break!
-KVER="6.16.7"	# Primary Kernel version
+KVER="6.16.8"	# Primary Kernel version
 PVER="-gentoo"	# Kernel "patch" version
 
 # Set this variable if your Kernel is not located under /usr/src/
